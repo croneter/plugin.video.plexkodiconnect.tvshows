d9743457d08d2f2379343ed3a1c441a00ab9b20e
croneter <croneter@users.noreply.github.com>
2021-12-05 14:31:37 +0100

    Use addon.xml `reuselanguageinvoker` to turn add-on snappier

--- default.py
+++ default.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # We need this in order to use add-on paths like
 # 'plugin://plugin.video.plexkodiconnect.MOVIES' in the Kodi video database
-###############################################################################
 from logging import getLogger
 import sys
 import os
@@ -19,12 +18,8 @@ sys.path.append(__base__)
 
 import transfer, loghandler
 
-###############################################################################
 loghandler.config()
 LOG = getLogger('PLEX.TVSHOWS')
-###############################################################################
-
-HANDLE = int(sys.argv[1])
 
 
 def play():
@@ -36,26 +31,26 @@ def play():
     if not sys.argv[2]:
         # Browsing to a tv show from a tv show info dialog - picked up
         # by kodimonitor.py and its method OnAdd
-        xbmcplugin.setResolvedUrl(HANDLE, False, xbmcgui.ListItem())
+        xbmcplugin.setResolvedUrl(int(sys.argv[1]), False, xbmcgui.ListItem())
         return
     else:
-        request = f'{sys.argv[2]}&handle={HANDLE}'
+        request = f'{sys.argv[2]}&handle={int(sys.argv[1])}'
         if 'resume:true' in sys.argv:
             request += '&resume=1'
         elif 'resume:false' in sys.argv:
             request += '&resume=0'
         transfer.plex_command(f'PLAY-{request}')
-    if HANDLE == -1:
+    if int(sys.argv[1]) == -1:
         # Handle -1 received, not waiting for main thread
         return
     # Wait for the result
     result = transfer.wait_for_transfer(source='main')
     if result is True:
-        xbmcplugin.setResolvedUrl(HANDLE, False, xbmcgui.ListItem())
+        xbmcplugin.setResolvedUrl(int(sys.argv[1]), False, xbmcgui.ListItem())
         # Tell main thread that we're done
         transfer.send(True, target='main')
     else:
-        xbmcplugin.setResolvedUrl(HANDLE, True, result)
+        xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, result)
 
 
 if __name__ == '__main__':
